bb21f8737b2641cdae2ea95dd93fa00765e4839a
Sheila Arenillas <sheila.arenillas@bosonit.com>
2022-07-27 11:00:33 +0200

    Assignments

---
+++ Assignments/ejercicio1.js
@@ -0,0 +1,6 @@
+// Almacenamiento de valores con el operador de asignación
+// Setup
+var a;
+a=7;
+
+// Only change code below this line
---
+++ Assignments/ejercicio2.js
@@ -0,0 +1,8 @@
+// Asignación del valor de una variable a otra
+// Setup
+var a;
+a = 7;
+var b;
+a=b;
+
+// Only change code below this line
