aa2ef97b50dd96daa2dafa986b642e93010db250
Sheila Arenillas <sheila.arenillas@bosonit.com>
2022-07-27 11:10:57 +0200

    Variables

---
+++ Variables var/ejercicio1.js
@@ -0,0 +1,3 @@
+//Explore las diferencias entre las palabras clave var y let
+let catName = "Oliver";
+let  catSound = "Meow!";
---
+++ Variables var/ejercicio2.js
@@ -0,0 +1,5 @@
+//Declarar una variable de solo lectura con la palabra clave const
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
